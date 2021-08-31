5d7ce2d91df03362117c5a32b8bdeeb0e195bd32
Brandon Roberts <brandon.roberts@qaclaims.com>
2021-08-31 12:45:32 -0400

    first commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Identity_JWT_API.Controllers;
+using Identity_JWT_API.DTOs;
+using Identity_JWT_API.Extensions;
+using Identity_JWT_API.Helpers;
+using Identity_JWT_API.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class UsersController : BaseApiController
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        public UsersController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery] UserParams userParams)
+        {
+            var users = await _unitOfWork.UserRepository.GetMembersAsync(userParams);
+
+            Response.AddPaginationHeader(users.CurrentPage, users.PageSize,
+                users.TotalCount, users.TotalPages);
+
+            return Ok(users);
+        }
+
+        [HttpGet("{username}", Name = "GetUser")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
+        {
+            return await _unitOfWork.UserRepository.GetMemberAsync(username);
+        }
+    }
+}
